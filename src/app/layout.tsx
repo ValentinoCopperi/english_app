dfa508b632e1aa0713fcf9a702ba509096c938b1
copep <valencope100@gmail.com>
2024-11-13 02:23:02 -0300

    fixed deploy 6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
- 
+
 
 
   return (
     <html lang="en">
+
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -39,25 +43,25 @@ export default function RootLayout({
         <main className="grid grid-cols-1 lg:grid-cols-4">
 
           <div className="hidden lg:block lg:fixed top-0 left-0 w-[20%]">
-              <SideBar  />
+            <SideBar />
           </div>
 
           <div className="col-span-1 lg:ml-[20%] lg:col-span-4">
 
             <div>
 
-              <NavBar  />
+              <NavBar />
 
             </div>
 
             <div className=" bg-black text-white">
               <div className="py-3" />
               <div className="flex">
-                <div className="px-3"/>
+                <div className="px-3" />
                 <div className="bg-[#1e1e1e] w-full rounded-tl-3xl min-h-screen rounded-tr-3xl">
                   {children}
                 </div>
-                <div className=" px-3"/>
+                <div className=" px-3" />
               </div>
             </div>
 
